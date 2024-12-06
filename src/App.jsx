2647d7e3df1aa39cc47ea20cbecd88d2c30ae720
Goutam Bhati <goutambhati183@gmail.com>
2024-12-06 14:40:16 +0530

    initializing the project and installing the dependencies

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+
+const App = () => {
+  return (
+    <div className=' text-red-500 text-xl'>Hello jee from App.jsx <Button>Button</Button></div>
+    
+
+  )
+}
+
+export default App
