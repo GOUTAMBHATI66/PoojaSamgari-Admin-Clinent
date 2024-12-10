a91408be687a73b8d93327ea1f04c323db2444a1
aloksingh9919 <aloksingh12527@gmail.com>
2024-12-10 22:23:40 +0530

    complate home page ui

--- src/components/client/footer/Footer.jsx
+++ src/components/client/footer/Footer.jsx
@@ -4,32 +4,32 @@ import { RiTwitterXFill } from "react-icons/ri";
 import { FaInstagram } from "react-icons/fa";
 const Footer = () => {
   return (
-    <footer className="bg-primary text-white py-8">
+    <footer className=" text-orange-600 py-8">
       <div className="container mx-auto text-center space-y-6">
         {/* Logo */}
         <div>
-          <h1 className="text-xl font-bold tracking-widest font-serif ">|| SWASTIK ||</h1>
+          <h1 className="text-2xl font-bold tracking-widest font-serif ">|| SWASTIK ||</h1>
         </div>
         <div className="flex justify-center space-x-8 text-sm">
-          <a href="#" className="hover:text-muted">
+          <a href="#" className="hover:text-orange-500">
             Terms of Service
           </a>
-          <a href="#" className="hover:text-muted">
+          <a href="#" className="hover:text-orange-500">
             Privacy Policy
           </a>
-          <a href="#" className="hover:text-muted">
+          <a href="#" className="hover:text-orange-500">
             Security
           </a>
         </div>
 
         <div className="flex justify-center space-x-6 text-xl">
-          <a href="#" className="hover:text-muted">
+          <a href="#" className="hover:text-orange-500">
           <RiTwitterXFill />
           </a>
-          <a href="#" className="hover:text-muted">
+          <a href="#" className="hover:text-orange-500">
             <FaFacebook />
           </a>
-          <a href="#" className="hover:text-muted">
+          <a href="#" className="hover:text-orange-500">
           <FaInstagram />
           </a>
         </div>
--- src/components/client/navbar/Navbar.jsx
+++ src/components/client/navbar/Navbar.jsx
@@ -3,12 +3,12 @@ import { FiUser } from "react-icons/fi";
 import { LuShoppingCart } from "react-icons/lu";
 const Navbar = () => {
   return (
-    <nav className="bg-white/90 sticky top-0 ">
+    <nav className="bg-white/90 sticky top-0 z-50">
       <div className="h-14 flex items-center justify-between container">
-        <div className="text-xl font-bold font-serif text-primary">|| SWASTIK ||</div>
+        <div className="text-xl font-bold font-serif text-orange-600">|| SWASTIK ||</div>
         <div className="flex items-center justify-center gap-x-4">
-          <FiUser size={23} className="text-primary hover:text-primary-foreground  cursor-pointer" />
-          <LuShoppingCart size={23} className="text-primary hover:text-primary-foreground cursor-pointer" />
+          <FiUser size={23} className="text-orange-600 hover:text-orange-500  cursor-pointer" />
+          <LuShoppingCart size={23} className="text-orange-600 hover:text-orange-500 cursor-pointer" />
         </div>
       </div>
     </nav>
