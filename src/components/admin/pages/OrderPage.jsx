479cc1c52da8138ec7238c8e9c9e5e1d4902b7c3
prsprshr@gmail.com <123573626+ParasParashar@users.noreply.github.com>
2024-12-13 01:18:36 +0530

    add the pagination in the OrderPage

--- src/components/admin/pages/OrderPage.jsx
+++ src/components/admin/pages/OrderPage.jsx
@@ -17,21 +17,28 @@ import {
 import AxiosBase from "@/lib/axios";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { Button } from "@/components/ui/button";
 
 const OrderPage = () => {
   const [orders, setOrders] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [deliveryStatuses, setDeliveryStatuses] = useState({});
+  const [page, setPage] = useState(1);
+  const [size, setSize] = useState(10);
+  const [totalPages, setTotalPages] = useState(0);
 
-  // Fetch orders with items
+  // Fetch orders with pagination
   const fetchOrders = async () => {
     try {
       setIsLoading(true);
-      const { data } = await AxiosBase.get("/api/admin/orders");
+      const { data } = await AxiosBase.get("/api/admin/orders", {
+        params: { page, size },
+      });
       if (!data.success)
         throw new Error(data.message || "Failed to fetch orders.");
       setOrders(data.data);
-      console.log(data);
+      setTotalPages(data.pagination.totalPages || 0);
+
       const statuses = data.data.reduce((acc, order) => {
         acc[order.id] = order.status;
         return acc;
@@ -52,12 +59,9 @@ const OrderPage = () => {
         [orderId]: newStatus,
       }));
 
-      // Send updated status to the server
       const { data } = await AxiosBase.put(
         `/api/admin/orders/${orderId}/status`,
-        {
-          status: newStatus,
-        }
+        { status: newStatus }
       );
 
       if (!data.success)
@@ -69,9 +73,14 @@ const OrderPage = () => {
     }
   };
 
+  const handlePageChange = (newPage) => {
+    console.log(page, "sldjflsjdf");
+    setPage(newPage);
+  };
+
   useEffect(() => {
     fetchOrders();
-  }, []);
+  }, [page, size]);
 
   return (
     <main>
@@ -91,81 +100,99 @@ const OrderPage = () => {
       )}
 
       {orders.length > 0 && !isLoading && (
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>S.No</TableHead>
-              <TableHead>Order ID</TableHead>
-              <TableHead>Customer Name</TableHead>
-              <TableHead>Shipping Address</TableHead>
-              <TableHead>Order Contact</TableHead>
-              <TableHead>Total Amount</TableHead>
-              <TableHead>Order Items</TableHead>
-              <TableHead>Payment Type</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Update Status</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {orders.map((order, index) => (
-              <TableRow key={order.id}>
-                <TableCell>{index + 1}</TableCell>
-                <TableCell>{order.id}</TableCell>
-                <TableCell>{order.user?.name || "N/A"}</TableCell>
-                <TableCell className="capitalize">
-                  {order.shippingAddress?.street}, {order.shippingAddress?.city}
-                  , {order.shippingAddress?.state},{" "}
-                  {order.shippingAddress?.postalCode},{" "}
-                  {order.shippingAddress?.country}
-                </TableCell>
-                <TableCell className="capitalize">
-                  <p>+{order.shippingAddress.phonenumber}</p>
-                  {order.shippingAddress.email}
-                </TableCell>
-                <TableCell>&#8377;{order.totalAmount.toFixed(2)}</TableCell>
-                <TableCell>{order.paymentMethod}</TableCell>
-                <TableCell>
-                  {order.orderItems.map((item) => (
-                    <div key={item.id} className="mb-2">
-                      <span className="font-medium">
-                        {item.product?.name || "Unknown Product"}
-                      </span>
-                      <span className="ml-2 text-gray-500">
-                        x{item.quantity}
-                      </span>
-                    </div>
-                  ))}
-                </TableCell>
-                <TableCell>{deliveryStatuses[order.id]}</TableCell>
-                <TableCell>
-                  <Select
-                    value={deliveryStatuses[order.id]}
-                    onValueChange={(newStatus) =>
-                      handleStatusChange(order.id, newStatus)
-                    }
-                  >
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select Status" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="PENDING">Pending</SelectItem>
-                      <SelectItem
-                        value="  SHIPPED
-"
-                      >
-                        Shipped
-                      </SelectItem>
-                      <SelectItem value="OUT_FOR_DELIVERY">
-                        Out for Delivery
-                      </SelectItem>
-                      <SelectItem value="DELIVERED">Delivered</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </TableCell>
+        <>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>S.No</TableHead>
+                <TableHead>Order ID</TableHead>
+                <TableHead>Customer Name</TableHead>
+                <TableHead>Shipping Address</TableHead>
+                <TableHead>Order Contact</TableHead>
+                <TableHead>Total Amount</TableHead>
+                <TableHead>Order Items</TableHead>
+                <TableHead>Payment Type</TableHead>
+                <TableHead>Payment Status</TableHead>
+                <TableHead>Update Status</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody>
+              {orders.map((order, index) => (
+                <TableRow key={order.id}>
+                  <TableCell>{(page - 1) * size + index + 1}</TableCell>
+                  <TableCell>{order.id}</TableCell>
+                  <TableCell>{order.user?.name || "N/A"}</TableCell>
+                  <TableCell className="capitalize">
+                    {order.shippingAddress?.street},{" "}
+                    {order.shippingAddress?.city},{" "}
+                    {order.shippingAddress?.state},{" "}
+                    {order.shippingAddress?.postalCode},{" "}
+                    {order.shippingAddress?.country}
+                  </TableCell>
+                  <TableCell className="capitalize">
+                    <p>+{order.shippingAddress.phonenumber}</p>
+                    {order.shippingAddress.email}
+                  </TableCell>
+                  <TableCell>&#8377;{order.totalAmount.toFixed(2)}</TableCell>
+                  <TableCell>{order.paymentMethod}</TableCell>
+                  <TableCell>
+                    {order.orderItems.map((item) => (
+                      <div key={item.id} className="mb-2">
+                        <span className="font-medium">
+                          {item.product?.name || "Unknown Product"}
+                        </span>
+                        <span className="ml-2 text-gray-500">
+                          x{item.quantity}
+                        </span>
+                      </div>
+                    ))}
+                  </TableCell>
+                  <TableCell>{deliveryStatuses[order.id]}</TableCell>
+                  <TableCell>
+                    <Select
+                      value={deliveryStatuses[order.id]}
+                      onValueChange={(newStatus) =>
+                        handleStatusChange(order.id, newStatus)
+                      }
+                    >
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select Status" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="PENDING">Pending</SelectItem>
+                        <SelectItem value="SHIPPED">Shipped</SelectItem>
+                        <SelectItem value="OUT_FOR_DELIVERY">
+                          Out for Delivery
+                        </SelectItem>
+                        <SelectItem value="DELIVERED">Delivered</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+
+          <div className="flex justify-between items-center mt-4">
+            <Button
+              className="btn"
+              disabled={page === 1}
+              onClick={() => handlePageChange(page - 1)}
+            >
+              Previous
+            </Button>
+            <p>
+              Page {page} of {totalPages}
+            </p>
+            <Button
+              className="btn"
+              disabled={page === totalPages}
+              onClick={() => handlePageChange(page + 1)}
+            >
+              Next
+            </Button>
+          </div>
+        </>
       )}
     </main>
   );
